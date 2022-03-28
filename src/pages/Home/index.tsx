1046dae291bb9267bd282c2f376a23e16c8ed34e
leords <leo_ro.drigues@hotmail.com>
2022-03-28 12:13:45 -0300

    Layout de login e de cadastro alterados

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -4,6 +4,8 @@ import { useNavigate } from "react-router-dom";
 
 import styles from "./styles.module.scss";
 
+import logo from "../../assets/icones/pokedex.png";
+
 import { Login } from "../Login";
 
 export function Home() {
@@ -22,12 +24,45 @@ export function Home() {
 
     return(
         <div className={styles.container}>
-            <h1>Leonardo</h1>
-            <button
-                onClick={singOut}
-            >
-                Sair
-            </button>
+
+            <div className={styles.header}>
+                <h1>Usuario</h1>
+                <button
+                    onClick={singOut}
+                >
+                    Sair
+                </button>
+            </div>
+
+            <div className={styles.bodyPokedex}>
+                <div className={styles.perfilPokemon}>
+                    <div className={styles.cardPoke}>
+
+                    </div>
+                </div>
+
+                <div className={styles.listPokemon}>
+                    <div className={styles.imageLogo}>
+                        <img src={logo} alt="" />
+                        <h1>Pokedex</h1>
+                    </div>
+                    <div className={styles.listPoke}>
+                        <div className={styles.tittle}>
+                            <p>Meus Pokemons</p>
+                        </div>
+                        <div className={styles.poke}>
+                            <div className={styles.imagePoke}>
+                                <img src={logo} alt="" />
+                            </div>
+                            <div className={styles.infoPoke}>
+                                <h1>Charizzard</h1>
+                                <p>Peso: 150oz</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
         </div>
     )
 }
